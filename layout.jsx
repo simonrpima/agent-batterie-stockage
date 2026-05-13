b052c2b31880aece7eb2122c1d9c2b5f006f8c2b
simonrpima <contact@batterie-stockage.fr>
2026-05-13 22:08:43 +0200

    Create layout.jsx

---
+++ layout.jsx
@@ -0,0 +1,9 @@
+export const metadata = { title: 'Agent Batterie Stockage' };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
